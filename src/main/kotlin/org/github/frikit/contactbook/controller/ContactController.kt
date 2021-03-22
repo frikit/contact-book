be1f8a0136716ca454cd761524361c4afe9f5481
frikit <osipovvictor1994@gmail.com>
2021-03-22 21:04:20 +0000

    add endpoint to add new contact

--- src/main/kotlin/org/github/frikit/contactbook/controller/ContactController.kt
+++ src/main/kotlin/org/github/frikit/contactbook/controller/ContactController.kt
@@ -2,14 +2,15 @@ package org.github.frikit.contactbook.controller
 
 import org.github.frikit.contactbook.model.Contact
 import org.github.frikit.contactbook.service.ContactService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class ContactController(
     private val contactService: ContactService
 ) {
+
+    private val log = LoggerFactory.getLogger(ContactController::class.java)
 
     @GetMapping("/contacts")
     fun getContacts(
@@ -19,6 +20,16 @@ class ContactController(
             contactService.findAllContacts()
         else
             contactService.findContactByPostcode(postcode)
+    }
+
+
+    @PostMapping("/add/contact")
+    fun addContact(
+        @RequestBody contactToAdd: Contact
+    ) {
+        log.info("Add contact=$contactToAdd...")
+        val added = contactService.saveContact(contactToAdd)
+        log.info("Add contact=$added...[OK]")
     }
 
 }
